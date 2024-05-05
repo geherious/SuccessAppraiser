280a3b427a92178eb3589dd441bac124aa61973d
geherious <vlaznev.04@mail.ru>
2024-05-05 17:04:33 +0300

    refactor: separate cell from body in calendar

--- react-ui/src/Components/Calendar/CalendarBody.js
+++ react-ui/src/Components/Calendar/CalendarBody.js
@@ -1,16 +1,11 @@
 import './CalendarBody.css';
 import clsx from 'clsx';
 import useCalendar from '../../hooks/useCalendar';
-import useGoal from "../../hooks/useGoal";
-import { getLastMonthDates, getDatesInMonth, getNextMonthDates, getLastDayInMonth, getWeekDay, getNewDateNoTime } from '../../Services/Calendar/calendarService';
+import { getLastMonthDates, getDatesInMonth, getNextMonthDates, getLastDayInMonth, getWeekDay } from '../../Services/Calendar/calendarService';
+import Cell from './Cell';
 
 const CalendarBody = () => {
-  const {currentDateArea} = useCalendar();
-
-  const {dates, activeGoal} = useGoal();
-
-  const weekDaysNames = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'];
-  const statelessColor = '#ADADAD';
+  const { currentDateArea } = useCalendar();
 
   const firstWeekDay = getWeekDay(currentDateArea, 1);
   const lastWeekDay = getWeekDay(getLastDayInMonth(currentDateArea), 1);
@@ -21,56 +16,13 @@ const CalendarBody = () => {
 
   const cells = lastMonthDates.concat(currentMonthDates, nextMonthDates);
 
-  const spanFromDate = (state = null, date) => {
-    const startDate = getNewDateNoTime(activeGoal.dateStart);
-    const goalEnd = getNewDateNoTime(activeGoal.dateStart);
-    goalEnd.setDate(startDate.getDate() + activeGoal.daysNumber);
-    const now = getNewDateNoTime();
-    const endDay = now <= goalEnd ? now : goalEnd;
-
-    let statusEl =
-    <span
-      style={state ? {backgroundColor: state.color} : {backgroundColor: statelessColor}}
-      className={clsx(startDate <= date && date <= endDay && 'cell-status')}
-    >
-    </span>
-    return statusEl
-  }
-
-  const createSpan = (dateShift, ind) => {
-    let statusEl;
-    if (activeGoal && dates && dates.length > 0){
-      if (dateShift < dates.length && cells[ind].getTime() === getNewDateNoTime(dates[dateShift].date).getTime()){
-        let status = activeGoal.template.states.find(state => state.id === dates[dateShift].stateId)
-        statusEl = spanFromDate(status, cells[ind])
-        dateShift++;
-      }
-      else {
-        statusEl = spanFromDate(null, cells[ind])
-      }
-    }
-    return statusEl;
-  }
-
 
   const cellsElements = cells => {
     let content = [];
     let dateShift = 0;
-    for (let i = 0; i < cells.length; i++){
-      let dateEl = cells[i].getDate();
-      let statusEl = createSpan(dateShift, i);
-
-      let item =
-      <div key={cells[i]} className='calendar-cell'>
-        {i < 7 && <span>{weekDaysNames[i]}</span>}
-
-        {dateEl === 1 ? 
-        <span>{dateEl + ' ' + cells[i].toLocaleString('en-us', { month: 'short'})}</span> :
-        <span>{dateEl}</span>
-        }
-
-        {statusEl ?? statusEl}
-      </div>
+    for (let i = 0; i < cells.length; i++) {
+      const date = cells[i];
+      const item = <Cell key={date} date={date} dateShift={dateShift} cellNumber={i}/>;
 
       content.push(item);
     }
---
+++ react-ui/src/Components/Calendar/Cell.js
@@ -0,0 +1,62 @@
+import './Cell.css';
+import useGoal from "../../hooks/useGoal";
+import clsx from 'clsx';
+import { getNewDateNoTime } from '../../Services/Calendar/calendarService';
+
+
+const Cell = ({ date, dateShift, cellNumber }) => {
+
+    const {activeGoal, dates} = useGoal();
+
+    const weekDaysNames = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'];
+    const statelessColor = '#ADADAD';
+
+    const spanFactory = (state = null) => {
+        const startDate = getNewDateNoTime(activeGoal.dateStart);
+        const goalEnd = getNewDateNoTime(activeGoal.dateStart);
+        goalEnd.setDate(startDate.getDate() + activeGoal.daysNumber);
+        const now = getNewDateNoTime();
+        const endDay = now <= goalEnd ? now : goalEnd;
+
+        let statusEl =
+            <span
+                style={state ? { backgroundColor: state.color } : { backgroundColor: statelessColor }}
+                className={clsx(startDate <= date && date <= endDay && 'cell-status')}
+            >
+            </span>
+        return statusEl
+    }
+
+    const createStatusSpan = () => {
+        let statusEl;
+        if (activeGoal && dates && dates.length > 0) {
+            if (dateShift < dates.length && date.getTime() === getNewDateNoTime(dates[dateShift].date).getTime()) {
+                let status = activeGoal.template.states.find(state => state.id === dates[dateShift].stateId)
+                statusEl = spanFactory(status)
+                dateShift++;
+            }
+            else {
+                statusEl = spanFactory(null)
+            }
+        }
+        return statusEl;
+    }
+
+    const dateEl = date.getDate();
+    const statusEl = createStatusSpan();
+
+    return (
+        <div className='calendar-cell'>
+            {cellNumber < 7 && <span>{weekDaysNames[cellNumber]}</span>}
+
+            {dateEl === 1 ?
+                <span>{dateEl + ' ' + date.toLocaleString('en-us', { month: 'short' })}</span> :
+                <span>{dateEl}</span>
+            }
+
+            {statusEl ?? statusEl}
+        </div>
+    )
+}
+
+export default Cell
