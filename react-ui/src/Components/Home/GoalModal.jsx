f8ab6a654024020f17bda35e07a9ed9eb86f1187
geherious <vlaznev.04@mail.ru>
2024-07-27 17:04:44 +0300

    feat-ui: template auto get

--- react-ui/src/Components/Home/GoalModal.jsx
+++ react-ui/src/Components/Home/GoalModal.jsx
@@ -5,9 +5,11 @@ import './DateModal.css';
 import 'react-toastify/dist/ReactToastify.css';
 import useAxiosPrivate from '../../hooks/useAxiosPrivate';
 import { getDateOnlyString } from '../../Services/Calendar/calendarService';
-import { goalsUrlEndpoint } from '../../api/goalApi';
+import { goalsUrlEndpoint, templatesUrlEndpoint } from '../../api/goalApi';
 import { toast } from 'react-toastify';
 import useNewGoal from '../../hooks/useNewGoal';
+import useSWR from 'swr';
+import LoaderDots from '../Loaders/LoaderDots';
 
 
 const GoalModal = () => {
@@ -15,6 +17,7 @@ const GoalModal = () => {
   const [description, setDescription] = useState('');
   const [numberOfDays, setNumberOfDays] = useState(21);
   const [startDate, setStartDate] = useState(getDateOnlyString(new Date()));
+  const [templateId, setTemplateId] = useState('');
 
   const isActive = useHomeStore(state => state.goalModalIsActive);
   const setIsActive = useHomeStore(state => state.setGoalModalIsActive);
@@ -24,6 +27,11 @@ const GoalModal = () => {
 
   const axiosPrivate = useAxiosPrivate();
   const { mutateGoals } = useNewGoal();
+
+  const fetchSettings = { revalidateOnFocus: false, revalidateIfStale: false, shouldRetryOnError: false };
+  const {
+    data: templates
+  } = useSWR(templatesUrlEndpoint, axiosPrivate.get, fetchSettings);
 
   useEffect(() => {
     if (isActive) {
@@ -35,20 +43,20 @@ const GoalModal = () => {
   }, [isActive])
 
   useEffect(() => {
-    if (isLoading) {
-      submitButton.current.disabled = true;
-    } else {
-      submitButton.current.disabled = false;
+    if (templates) {
+      setTemplateId(templates.data[0].id);
     }
-  }, [isLoading])
+  }, [templates])
 
   const submitForm = async (e) => {
     setIsloading(true);
     e.preventDefault();
-    // TODO: get templates from server
-    const newData = { name: name, description: description, daysNumber: numberOfDays,
-      dateStart: startDate, templateId: '1896477c-94ea-4b04-b523-75b382328e88'}
-    try{
+    
+    const newData = {
+      name: name, description: description, daysNumber: numberOfDays,
+      dateStart: startDate, templateId: '1896477c-94ea-4b04-b523-75b382328e88'
+    }
+    try {
       const response = await axiosPrivate.post(goalsUrlEndpoint, JSON.stringify(newData));
       mutateGoals(response.data);
     }
@@ -56,42 +64,55 @@ const GoalModal = () => {
       console.log(error);
       toast.error('Something went wrong');
     }
-    finally{
+    finally {
       setIsloading(false);
       setIsActive(false);
     }
   }
 
   return (
-    <ModalBase isActive={isActive} setIsActive={setIsActive}>
-        <form onSubmit={submitForm}>
-          <div className='base-modal-header'>
-            <span>Create new goal</span>
-          </div>
+    <>
+      {templates ? (
+        <ModalBase isActive={isActive} setIsActive={setIsActive}>
+          <form onSubmit={submitForm}>
+            <div className='base-modal-header'>
+              <span>Create new goal</span>
+            </div>
 
-          <div className='base-modal-body'>
-            <label htmlFor='goal-modal-name-input'>Name</label>
-            <input id='goal-modal-name-input' type='text' autoComplete='off' value={name} onChange={e => setName(e.target.value)} required/>
+            <div className='base-modal-body'>
+              <label htmlFor='goal-modal-name-input'>Name</label>
+              <input id='goal-modal-name-input' type='text' autoComplete='off' value={name} onChange={e => setName(e.target.value)} required />
 
-            <label htmlFor='goal-modal-description-input'>Description (optional)</label>
-            <input id='goal-modal-description-input' type='text' autoComplete='off' value={description} onChange={e => setDescription(e.target.value)}/>
+              <label htmlFor='goal-modal-description-input'>Description (optional)</label>
+              <input id='goal-modal-description-input' type='text' autoComplete='off' value={description} onChange={e => setDescription(e.target.value)} />
 
-            <label htmlFor='goal-modal-days-number-input'>Number of days</label>
-            <input id='goal-modal-days-number-input' type='number' min='1' value={numberOfDays} onChange={e => setNumberOfDays(e.target.value)} required/>
+              <label htmlFor='goal-modal-days-number-input'>Number of days</label>
+              <input id='goal-modal-days-number-input' type='number' min='1' value={numberOfDays} onChange={e => setNumberOfDays(e.target.value)} required />
 
-            <label htmlFor='goal-modal-date-start-input'>Start date</label>
-            <input id='goal-modal-date-start-input' type='date' min='1950-12-12' value={startDate} onChange={e => setStartDate(e.target.value)} required/>
+              <label htmlFor='goal-modal-date-start-input'>Start date</label>
+              <input id='goal-modal-date-start-input' type='date' min='1950-12-12' value={startDate} onChange={e => setStartDate(e.target.value)} required />
 
-            <label htmlFor='goal-modal-template-input'>Template</label>
-            <input disabled={true} id='goal-modal-template-input' type='text' value="Habbit" required/>
-          </div>
+              <label htmlFor='goal-modal-template-input'>Template</label>
+              <select id='goal-modal-template-input' value={templateId} onChange={e => setTemplateId(e.target.value)}>
+                {templates.data.map(template => (
+                  <option key={template.id} value={template.id}>{template.name}</option>
+                ))}
+              </select>
+            </div>
 
-          <div className="base-modal-footer">
-            <button type="submit" disabled={isLoading} ref={submitButton}>Save</button>
-            <button type="button" onClick={() => setIsActive(false)}>Cancel</button>
-          </div>
-        </form>
-    </ModalBase>
+            <div className="base-modal-footer">
+              <button type="submit" disabled={isLoading} ref={submitButton}>Save</button>
+              <button type="button" onClick={() => setIsActive(false)}>Cancel</button>
+            </div>
+          </form>
+        </ModalBase>) :
+        (
+          <ModalBase isActive={isActive} setIsActive={setIsActive}>
+            <LoaderDots />
+          </ModalBase>
+        )
+      }
+    </>
   )
 }
 
--- react-ui/src/api/goalApi.js
+++ react-ui/src/api/goalApi.js
@@ -2,3 +2,5 @@
 export const goalsUrlEndpoint = '/goals'
 
 export const goalDateEndpoint = (goalId) => `${goalsUrlEndpoint}/${goalId}/dates`
+
+export const templatesUrlEndpoint = '/templates'
